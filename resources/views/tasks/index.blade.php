36cab38a1571226b3e55d6d75066be4c322a286d
Peter Shackelford <peter.shackelford@nelnet.net>
2024-01-10 12:46:06 -0500

    disclaimer and htmx image, and readme

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -8,6 +8,9 @@
     @include('tasks.count')
     @include('tasks.new')
     @include('tasks.tasks')
+    <div style="text-align: center;margin:32px">
+        <img width="90%" loading="lazy" src="{{ asset('resources/images/createdwith.jpeg') }}">
+    </div>
+    <p>This site is a playground for learning HTMX. It is not intended to be a production site. It could be deleted/destroyed at any moment. It is not intended to be a secure or fast site. It is intended to be a site that demonstrates the power of HTMX.</p>
 @endsection
-
 
