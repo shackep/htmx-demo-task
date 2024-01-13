26a182dc7313cf36b685875e49e3af84be50a87b
Peter Shackelford <peter.shackelford@nelnet.net>
2024-01-13 09:20:00 -0500

    Fixed ordering

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -10,22 +10,18 @@ class TaskController extends Controller
 {
     public function index($group_key = null)
     {
-            // If a group_key is provided in the URL
-    
+        // If a group_key is provided in the URL
+
         // Check if tasks with that group_key exist
         if ($group_key && Task::where('group_key', $group_key)->exists()) {
             // If they do, set it as the session group_key
             session(['group_key' => $group_key]);
             $uniqueid = $group_key;
-        } else {
-        // If no group_key is provided in the URL, proceed as before
-
-            do {
-                $uniqueid = Str::random(8);
-            } while (Task::where('group_key', $uniqueid)->exists());
-
+        } elseif(!session('group_key')) {
+            // If no group_key is provided in the URL one does not exist in the session, generate a new one
+            $uniqueid = $this->generateUniqueGroupKey();
             session(['group_key' => $uniqueid]);
-    }
+        }
 
         $tasks = Task::when(
             request('filter') == 'remaining',
@@ -38,9 +34,18 @@ class TaskController extends Controller
                 return $query->where('done', 1);
             }
         )->where('group_key', $uniqueid)->get();
-        
+
         $count = $tasks->count();
-        return view('tasks.index', compact('tasks','count'));
+        return view('tasks.index', compact('tasks', 'count'));
+    }
+
+    private function generateUniqueGroupKey()
+    {
+        do {
+            $uniqueid = Str::random(8);
+        } while (Task::where('group_key', $uniqueid)->exists());
+
+        return $uniqueid;
     }
 
     public function store(Request $request)
@@ -49,7 +54,9 @@ class TaskController extends Controller
             'name' => $request->input('name'),
             'group_key' => $request->input('group_key')
         ]);
-        $count = Task::where('group_key', $request->input('group_key')
+        $count = Task::where(
+            'group_key',
+            $request->input('group_key')
         )->count();
         $task->count = true;
         return response(view('tasks.task', compact('task', 'count')), 200);
@@ -75,7 +82,7 @@ class TaskController extends Controller
         } else {
             $task->done = !$task->done;
         }
-        
+
         $task->save();
         return response(view('tasks.task', compact('task')), 200);
     }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,12 +2,23 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Task extends Model
 {
     use HasFactory;
     protected $fillable = ['name','done','order','group_key'];
+
+    protected static function boot()
+{
+    parent::boot();
+
+    static::addGlobalScope('crated_at', function (Builder $builder) {
+        $builder->orderBy('created_at', 'desc');
+    });
+}
     
 }
