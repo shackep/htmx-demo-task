7e18a6012605bb8d2e1a0033f5cf1a07de1cca7f
Peter Shackelford <peter.shackelford@nelnet.net>
2024-01-16 09:46:45 -0500

    trying missing css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,8 @@
 <html lang="en">
 <script src="https://unpkg.com/htmx.org@1.9.10"
  crossorigin="anonymous"></script> 
- <link rel="stylesheet" href="https://unpkg.com/simpledotcss/simple.min.css">
+ <!-- <link rel="stylesheet" href="https://unpkg.com/simpledotcss/simple.min.css"> -->
+ <link rel="stylesheet" href="https://unpkg.com/missing.css@1.1.1">
 <script>htmx.config.useTemplateFragments = true;</script>
 <head>
     <title>{{ $list_name }}</title>
