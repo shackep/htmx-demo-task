55e78e0b1f8fc420e246ff7d1e5b990bb4a6f77d
Peter Shackelford <peter.shackelford@nelnet.net>
2024-01-15 22:37:40 -0500

    actual spinner

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,5 +1,5 @@
 <form>
 <input type="text" name="name" value="{{$task->name}}" autocomplete="off" required>
 <button hx-indicator="#spinner" type="button" hx-patch="{{ route('tasks.update', $task) }}" hx-target="closest tr"
-            hx-swap="outerHTML">Save</button>
+            hx-swap="outerHTML">Save <span id='spinner' class="htmx-indicator">⏳</span></button>
 </form>
--- resources/views/tasks/new.blade.php
+++ resources/views/tasks/new.blade.php
@@ -1,7 +1,7 @@
 <form id="task-form" autocomplete="off">
         <input type="text" name="name" placeholder="New Task" autocomplete="off" required>
         <input type="hidden" name="group_key" value="{{ session('group_key') }}">
-        <button hx-indicator="#spinner" type="button" hx-post="{{ route('tasks.store') }}" hx-target='#task-list' hx-swap='afterbegin'>Add Task</button>
+        <button hx-indicator="#spinner" type="button" hx-post="{{ route('tasks.store') }}" hx-target='#task-list' hx-swap='afterbegin'>Add Task <span id='spinner' class="htmx-indicator">⏳</span></button>
     </form>
 <script>
         // Clear the form input after successful post
