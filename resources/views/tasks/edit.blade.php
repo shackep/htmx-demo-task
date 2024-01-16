3c9de0d0e7c79eef4a9f17a4e069d8333f613f23
Peter Shackelford <peter.shackelford@nelnet.net>
2024-01-15 22:31:34 -0500

    added spinners

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,5 +1,5 @@
 <form>
 <input type="text" name="name" value="{{$task->name}}" autocomplete="off" required>
-<button type="button" hx-patch="{{ route('tasks.update', $task) }}" hx-target="closest tr"
+<button hx-indicator="#spinner" type="button" hx-patch="{{ route('tasks.update', $task) }}" hx-target="closest tr"
             hx-swap="outerHTML">Save</button>
 </form>
--- resources/views/tasks/new.blade.php
+++ resources/views/tasks/new.blade.php
@@ -1,7 +1,7 @@
 <form id="task-form" autocomplete="off">
         <input type="text" name="name" placeholder="New Task" autocomplete="off" required>
         <input type="hidden" name="group_key" value="{{ session('group_key') }}">
-        <button type="button" hx-post="{{ route('tasks.store') }}" hx-target='#task-list' hx-swap='afterbegin'>Add Task</button>
+        <button hx-indicator="#spinner" type="button" hx-post="{{ route('tasks.store') }}" hx-target='#task-list' hx-swap='afterbegin'>Add Task</button>
     </form>
 <script>
         // Clear the form input after successful post
