936b46b08ceb86bb4f7fe746ef29d5ca4464a7d6
Peter Shackelford <peter.shackelford@nelnet.net>
2024-01-16 09:21:39 -0500

    added github link

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -21,6 +21,6 @@
     <div style="text-align: center;margin:32px">
         <img width="90%" loading="lazy" src="{{ asset('resources/images/createdwith.jpeg') }}">
     </div>
-    <p>This site is a playground for learning HTMX. It is not intended to be a production site. It could be deleted/destroyed at any moment. It is not intended to be a secure or fast site. It is intended to be a site that demonstrates the power of HTMX.</p>
+    <p>This site is a playground for learning HTMX. It is not intended to be a production site. It could be deleted/destroyed at any moment. It is not intended to be a secure or fast site. It is intended to be a site that demonstrates the power of HTMX. See it on github here: <a href="https://github.com/shackep/htmx-demo-task">Github</a></p>
 @endsection
 
